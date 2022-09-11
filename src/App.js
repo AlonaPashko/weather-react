5186b28ac6e9697db3a7f6c2ea1d3af6b62dc23f
AlonaPashko <105510952+AlonaPashko@users.noreply.github.com>
2022-09-11 15:03:38 +0200

    try to deploy

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import "./App.css";
 import Weather from "./Weather.js";
+import React from "react";
 
 export default function App() {
   return (
@@ -15,9 +16,9 @@ export default function App() {
         description="Cloudy"
       />
       <footer>This project was coded by{" "} 
-        <a href="https://www.linkedin.com/in/alona-pashko/" target="blank" rel="noreferrer">Alona Pashko</a> 
-        {" "}and is <a href="https://github.com/AlonaPashko/weather-react" target="_blanck" rel="noreferrer">open-sourced on GitHub</a> 
-        {" "}and <a href="https://shimmering-kangaroo-954530.netlify.app/" target="_blank" rel="noreferrer">hosted on Netlify </a>
+        <a href="https://www.linkedin.com/in/alona-pashko/" target="blank" rel="noopener noreferrer">Alona Pashko</a> 
+        {" "}and is <a href="https://github.com/AlonaPashko/weather-react" rel="noopener noreferrer" target="_blank">open-sourced on GitHub</a> 
+        {" "}and <a href="https://shimmering-kangaroo-954530.netlify.app/" target="_blank" rel="noopener noreferrer">hosted on Netlify </a>
         </footer>
     </div>
     </div>
